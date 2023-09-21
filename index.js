c5bed2ac8144c11a9bcc0dc4279c5e18a8b53fcc
olgakahraman <ol.khrmn@gmail.com>
2023-09-21 12:05:10 -0400

    added event.preventDefault() for consentForm

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const modal = document.getElementById('modal');
-const modalCloseBtn = document.getElementById('modal-close-btn');
+const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
 
+setTimeout(function () {
+  modal.style.display = "inline";
+}, 1500);
 
-setTimeout(function(){
-    modal.style.display = 'inline';
-}, 1500)
+modalCloseBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
 
-modalCloseBtn.addEventListener('click', function(){
-    modal.style.display = 'none';
+const consentForm = document.querySelector("#consent-form");
+
+consentForm.addEventListener("submit", function(event){
+  event.preventDefault();
+  console.log("form submitted");
 })
