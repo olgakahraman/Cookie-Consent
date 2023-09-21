db9ceb0680f534edab4128007f8a4200929f91df
olgakahraman <ol.khrmn@gmail.com>
2023-09-21 12:43:53 -0400

    added loading animation with svg and changing text with setTimeout()

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.querySelector("#consent-form");
+const modalText = document.querySelector("#modal-text");
+
+
 
 setTimeout(function () {
   modal.style.display = "inline";
 }, 1500);
 
+
 modalCloseBtn.addEventListener("click", function () {
   modal.style.display = "none";
 });
 
-const consentForm = document.querySelector("#consent-form");
-
 consentForm.addEventListener("submit", function(event){
   event.preventDefault();
-  console.log("form submitted");
+  modalText.innerHTML = `
+  <div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div> 
+  `;
+setTimeout(function(){
+
+const uploadText = document.getElementById("uploadText");
+uploadText.innerHTML = `Making the sale...`;
+}, 1500);
+
 })
+
+
