4aa5b61396b3513bb7346d150f343b128f7e9a1b
olgakahraman <ol.khrmn@gmail.com>
2023-09-23 11:16:27 -0400

    added 3rd message with gif

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
 const consentForm = document.querySelector("#consent-form");
 const modalText = document.querySelector("#modal-text");
-
+const modalInner = document.querySelector("#modal-inner");
 
 
 setTimeout(function () {
   modal.style.display = "inline";
 }, 1500);
-
 
 modalCloseBtn.addEventListener("click", function () {
   modal.style.display = "none";
@@ -18,17 +17,29 @@ consentForm.addEventListener("submit", function(event){
   event.preventDefault();
   modalText.innerHTML = `
   <div class="modal-inner-loading">
-    <img src="images/loading.svg" class="loading">
+    <img src="images/loading.svg" class="loading" id="loading">
     <p id="uploadText">
         Uploading your data to the dark web...
     </p>
 </div> 
   `;
 setTimeout(function(){
-
-const uploadText = document.getElementById("uploadText");
-uploadText.innerHTML = `Making the sale...`;
+document.getElementById("uploadText").innerText = `Making the sale...`;
 }, 1500);
+
+
+setTimeout(function(){
+
+  consentForm.style.display="none";
+  modalInner.innerHTML = `
+   <h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>
+    
+  `;
+},3000);
 
 })
 
